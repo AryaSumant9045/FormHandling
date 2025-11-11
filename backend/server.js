b7a6253c384470f028d4265786ffb83616f4e9ce
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-11 13:34:03 +0530

    auto update 2025-11-11 13:34:03

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.post('/', (req, res) => {
-    console.log(req.form)
+    // console.log(req.form)
   res.send('Hello World!')
 })
 app.listen(port, () => {
